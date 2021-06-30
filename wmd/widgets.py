99f7c9e640ce9ca598fe121e4f56794fdca28045
morgan <morgan@txrxlabs.org>
2021-06-30 14:32:22 -0500

    Updating to latest Django/Python if possible

--- wmd/widgets.py
+++ wmd/widgets.py
@@ -1,16 +1,17 @@
-
 from django import forms
 from django.conf import settings
+
 try:
-    from django.forms.utils import flatatt # Renamed in Django 1.7
+    from django.forms.utils import flatatt  # Renamed in Django 1.7
 except ImportError:
-    from django.forms.util import flatatt # Removed in Django 1.9
+    from django.forms.utils import flatatt  # Removed in Django 1.9
 from django.utils.html import escape
-from django.utils.encoding import force_unicode
+# from django.utils.encoding import force_unicode
 from django.utils.safestring import mark_safe
 from django.contrib.admin import widgets as admin_widgets
 
 from wmd import settings as wmd_settings
+
 
 class MarkDownInput(forms.Textarea):
 
@@ -22,20 +23,21 @@ class MarkDownInput(forms.Textarea):
         final_attrs = self.build_attrs(attrs, name=name)
 
         html = [u'<div class="wmd-panel"><div id="wmd-button-bar"></div><textarea%s>%s</textarea></div>' % \
-                (flatatt(final_attrs), force_unicode(escape(value)))]
+                (flatatt(final_attrs), str(escape(value)))]
 
         if wmd_settings.WMD_SHOW_PREVIEW:
-            #TODO: Maybe we can generate ids here if there are more than one editor?
+            # TODO: Maybe we can generate ids here if there are more than one editor?
             html.append(u'<div class="wmd-panel"><div id="wmd-preview"></div></div>')
 
         return mark_safe(u'\n'.join(html))
 
     def _media(self):
-        return forms.Media(css= {'screen': [settings.STATIC_URL + "wmd/wmd.css"]},
+        return forms.Media(css={'screen': [settings.STATIC_URL + "wmd/wmd.css"]},
                            js=(settings.STATIC_URL + "wmd/showdown.js",
                                settings.STATIC_URL + "wmd/wmd.js"))
 
     media = property(_media)
+
 
 class AdminMarkDownInput(admin_widgets.AdminTextareaWidget, MarkDownInput):
     # The admin input has its own attribute to show the preview or not
@@ -47,7 +49,7 @@ class AdminMarkDownInput(admin_widgets.AdminTextareaWidget, MarkDownInput):
         final_attrs = self.build_attrs(attrs, name=name)
 
         html = [u'<div class="wmd-panel"><div id="wmd-button-bar"></div><textarea%s>%s</textarea></div>' % \
-                (flatatt(final_attrs), force_unicode(escape(value)))]
+                (flatatt(final_attrs), str(escape(value)))]
 
         if wmd_settings.WMD_ADMIN_SHOW_PREVIEW:
             html.append(u'<div class="wmd-panel wmd-admin"><div id="wmd-preview"></div></div>')
